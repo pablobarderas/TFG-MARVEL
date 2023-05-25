7487e87c39d2f0c11b4f5ee2e95b138119bc7ca1
pablobarderas <pbarderas31@gmail.com>
2023-05-25 16:07:18 +0200

    Add creators service

--- e_lapse_project/e_lapse_marvel/api_services.py
+++ e_lapse_project/e_lapse_marvel/api_services.py
@@ -81,7 +81,35 @@ def get_comics_list(titleStartsWith):
     return get_data_list(BASE_URL, args)
 
 
+# GET CREATORS LIST
+def get_creators_list(nameStartsWith):
+    BASE_URL = "https://gateway.marvel.com/v1/public/creators"
+    hash = getHash(1000)
+    args = {
+        'nameStartsWith': nameStartsWith,
+        'ts': 1000,
+        'apikey': 'dbc203265f84033afd469a493cec6b27',
+        'hash': hash,
+        'limit': 20,
+        'offset': 0
+    }
+    return get_data_list(BASE_URL, args)
+
+
+# GET CREATORS BY ID
+def get_creator_by_id(creator_id):
+    BASE_URL = "https://gateway.marvel.com/v1/public/creators/" + creator_id
+    hash = getHash(1000)
+    args = {
+        'ts': 1000,
+        'apikey': 'dbc203265f84033afd469a493cec6b27',
+        'hash': hash,
+    }
+    return get_data_list(BASE_URL, args)
+
 # GET CHARACTER BY ID
+
+
 def get_character_by_id(character_id):
     BASE_URL = "https://gateway.marvel.com/v1/public/characters/" + character_id
     hash = getHash(1000)
