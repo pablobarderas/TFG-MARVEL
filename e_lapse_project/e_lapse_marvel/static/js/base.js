7762e77e1d66237b92237902a9ef1ccc9a0b6060
pablobarderas <pbarderas31@gmail.com>
2023-06-06 01:03:35 +0200

    new slider view

--- e_lapse_project/e_lapse_marvel/static/js/base.js
+++ e_lapse_project/e_lapse_marvel/static/js/base.js
@@ -14,7 +14,21 @@ $(document).ready(function() {
         });
     });
 
-    // CHARACTER
-    
-
   });
+
+/*
+  $(window).on('load', function() {
+       // Mostrar el spinner
+       $('#spinners').removeClass('visually-hidden');
+
+        const currentUrl = window.location;
+        console.log(currentUrl.pathname);
+
+       // Realizar la solicitud GET
+       $.get(currentUrl, function() {
+           // Ocultar el spinner cuando se obtienen los datos
+           $('#spinners').addClass('visually-hidden');
+       });
+  });
+
+  */
